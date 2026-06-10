1919aa1413750c1499dd3fa0ba94b9885e7d72fd
Roland Rabien <figbug@gmail.com>
2026-06-10 11:59:34 -0700

    Add resources into macOS bundle

--- Source/Globals/constants.cpp
+++ Source/Globals/constants.cpp
@@ -284,7 +284,7 @@ juce::Path& UI::getScaledPathWithSize ( const juce::String& resourceName, juce::
 juce::File paths::getDataRoot ( juce::String path )
 {
 	#if JUCE_MAC
-		auto	ret = juce::File::getSpecialLocation ( juce::File::commonApplicationDataDirectory ).getChildFile ( "Application Support/ultraView" );
+		auto	ret = juce::File::getSpecialLocation ( juce::File::currentApplicationFile ).getChildFile ( "Contents/Resources/Data" );
 	#elif JUCE_WINDOWS
 		auto	ret = juce::File::getSpecialLocation ( juce::File::commonApplicationDataDirectory ).getChildFile ( "ultraView" );
 	#elif JUCE_LINUX
