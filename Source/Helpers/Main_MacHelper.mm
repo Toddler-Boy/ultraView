007bebf1b340b56c86d1762a0cb6350643bf2b4a
Roland Rabien <figbug@gmail.com>
2026-06-10 10:08:40 -0700

    Implement setWindowProperties for macOS

--- Source/Helpers/Main_MacHelper.mm
+++ Source/Helpers/Main_MacHelper.mm
@@ -2,12 +2,23 @@
 #include <Cocoa/Cocoa.h>
 #import <Foundation/Foundation.h>
 
-void setWindowProperties ( void* windowHandle )
+void setWindowProperties ( void* windowHandle, unsigned int titleColor )
 {
 	NSOperatingSystemVersion	tahoeVersion = { .majorVersion = 26, .minorVersion = 0, .patchVersion = 0 };
 	bool	isTahoe = [[NSProcessInfo processInfo] isOperatingSystemAtLeastVersion:tahoeVersion];
 
 	auto view = (NSView*) windowHandle;
 	view.layer.cornerRadius = isTahoe ? 20.0f : 10.0f;
+
+	NSWindow* window = [view window];
+	if ( window )
+	{
+		CGFloat r = ( ( titleColor >> 16 ) & 0xFF ) / 255.0;
+		CGFloat g = ( ( titleColor >> 8  ) & 0xFF ) / 255.0;
+		CGFloat b = ( ( titleColor       ) & 0xFF ) / 255.0;
+
+		window.titlebarAppearsTransparent = YES;
+		window.backgroundColor = [NSColor colorWithRed:r green:g blue:b alpha:1.0];
+	}
 }
 #endif
