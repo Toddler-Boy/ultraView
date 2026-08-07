b528d8eff2b4ba6e0b02b5c972dcb8ac04a2df4e
Michael Hartmann <mike@ultrasid.com>
2026-08-07 16:33:08 -0700

    Switched to development build

--- build_development.sh
+++ build_development.sh
@@ -9,4 +9,4 @@ trap 'handle_error $LINENO' ERR
 
 [ "$(uname)" == "Darwin" ] && TOOLCHAIN="xcode" || TOOLCHAIN="vs"
 
-cmake --build --preset $TOOLCHAIN --config Release --parallel
+cmake --build --preset $TOOLCHAIN --config Development --parallel
